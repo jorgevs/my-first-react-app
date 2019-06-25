8c43a4ca3b752f95185aea1b8ee3e780310f8578
jorgevs.mx <jorgevs.mx@gmail.com>
2019-06-25 19:29:01 -0400

    version3

--- src/index.js
+++ src/index.js
@@ -4,15 +4,40 @@ import ReactDOM from "react-dom";
 import Animal from './Animal/Animal'
 import Person from './Person/Person'
 
-var app = (
-    <div>
-        <Person name="Max" age="12"/>
-        <h1><Animal animalName="Tobby"/>guauuuu!</h1>
-        <br/>
-        <Person name="Didier" age="19"/>
-        <h1><Animal animalName="Buppys"/>miauuuu!</h1>
-    </div>
-);
+class App extends Component {
+    state = {
+        persons: [
+            {name: 'Max', age: 28},
+            {name: 'Didier', age: 29}
+        ],
+        otherState: 'some other value'
+    }
 
-ReactDOM.render(app, document.getElementById('root'));
+    switchNameHandler = () => {
+        // DON'T DO THIS: this.state.persons[0].name = "Maximiliano";
+        // DON'T DO THIS:this.state.persons[1].name = "Juan Carlos";
+
+        this.setState({
+            persons: [
+                {name: "Maximiliano", age: 28},
+                {name: "Juan Carlos", age: 29}
+            ]
+        })
+    }
+
+    render(){
+        return (
+            <div>
+                <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+                <h1><Animal animalName="Tobby"/>guauuuu!</h1>
+                <br/>
+                <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+                <h1><Animal animalName="Buppys"/>miauuuu!</h1>
+                <button onClick={this.switchNameHandler}>Switch name</button>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
